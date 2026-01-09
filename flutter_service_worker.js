ecb5df7fcb45df6198600050428aca4e4524f280
bramp <bramp@users.noreply.github.com>
2026-01-09 08:27:48 +0000

    deploy: 5da5d3a684216dce891186cc716123792530c797

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "ec56c684c4487881486d97263aa9150f",
 "favicon.ico": "2cf6d927e2c5e2ba6303f1c003c742c2",
-"flutter_bootstrap.js": "7687cc9b02d4e0abf1c2b97685f2027a",
+"flutter_bootstrap.js": "72625f5945dddf2e61a1ee82141ef6f1",
 "version.json": "34c0b4f0e8a3c76e57563dbf6cee99ae",
-"main.dart.js": "5fa54eff0c51429fc8f7f4d9b0b1c88f"};
+"main.dart.js": "0c292e93ef28cda73040f90b23c0dab4"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
