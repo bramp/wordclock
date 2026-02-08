d014299d1327c10a5212e79e1d2b89d979256025
bramp <bramp@users.noreply.github.com>
2026-02-08 17:13:25 +0000

    deploy: 4928ef4c64f6a1a5e89deedfb50cca207cb13b2a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3461688112"
+    serviceWorkerVersion: "3370281154"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "robots.txt": "7d1fe4998cc0f9695ae28d0dbebb0c57",
 "favicon.ico": "2cf6d927e2c5e2ba6303f1c003c742c2",
 "sitemap.xml": "edd68319b7d9ca1b5c0c63376f1c2d48",
-"flutter_bootstrap.js": "756670f9a8d838619b7119b0171306f4",
+"flutter_bootstrap.js": "c5bb4bbc96e45d6b37bb71e419a9475b",
 "version.json": "34c0b4f0e8a3c76e57563dbf6cee99ae",
-"main.dart.js": "073fc65c5212c845d267598deca0c5c1"};
+"main.dart.js": "9f3300a84dd93b2ec0e9fb7206bb7dc3"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
